62271d45930070d2ec8321c6d559334af3ae4df9
Kgabo Maraka <kgabo.maraka@nmi.co.za>
2023-11-19 16:19:30 +0200

    Corrected the method name

--- ReverseWordsOrder/Program.cs
+++ ReverseWordsOrder/Program.cs
@@ -2,9 +2,9 @@
 using ReverseWordsOrder;
 
 string[] inputArray = { "this is a test", "foobar", "all your base" };
-ReverseWords1(3, inputArray);
+ReverseWords(3, inputArray);
 
-static void ReverseWords1(int testCases, string[] strings)
+static void ReverseWords(int testCases, string[] strings)
 {
     var reverser = new Reverser(new ReverseOrder(testCases, strings));
     reverser.Reverse();
